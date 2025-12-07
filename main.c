319ebd2565d1a545e93ea4bb818d3fe09fa8e6c6
NikiCubik12 <nikicubik@gmail.com>
2025-12-07 20:37:07 +0300

    Добавлен ввод

--- main.c
+++ main.c
@@ -3,98 +3,188 @@
 #include "sort.h"
 #include "locale.h"
 
+void input_deck(Deck *deck)
+{
+    int value;
+    char str[200];
+    printf("Введите числа через пробел: ");
+    if (fgets(str, sizeof(str), stdin) != NULL)
+    {
+        char *pointer = str;
+        int num, space;
+        while (sscanf(pointer, "%d%n", &num, &space) == 1)
+        {
+            add_to_end(num, deck);
+            pointer += space;
+        }
+    }
+}
+
+
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    FILE *List;
-    int number;
-    List = fopen("Lists.txt", "r");
-    if (List == NULL) 
-    {
-        printf("РћС€РёР±РєР°. РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р».\n");
-        return 1; 
-    }
-    printf("РЎС‡РёС‚С‹РІР°РµРј С‡РёСЃР»Р° РёР· С„Р°Р№Р»Р°:\n");
-
-    Deck *deck = make();
-    while (fscanf(List, "%d", &number) == 1) 
-    {
-        add_to_end(number, deck);
-    }
-    print_deck(deck);
-    printf("\n");
-
-    fclose(List);
-
     int option, action, is_working = 1;
-    printf("Р”РµРє Р·Р°РіСЂСѓР¶РµРЅ РёР· С„Р°Р№Р»Р°. Р’С‹Р±РµСЂРёС‚Рµ РґРµР№СЃС‚РІРёРµ РѕС‚ 1 РґРѕ 6:\n");
     
     while (is_working == 1)
     {
-        
-        printf("1. Р”РѕР±Р°РІР»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РІ РЅР°С‡Р°Р»Рѕ РґРµРєР°\n");
-        printf("2. РЈРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РёР· РЅР°С‡Р°Р»Р° РґРµРєР°\n");
-        printf("3. Р”РѕР±Р°РІР»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РІ РєРѕРЅРµС† РґРµРєР°\n");
-        printf("4. РЈРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РёР· РєРѕРЅС†Р° РґРµРєР°\n");
-        printf("5. Р”РѕР±Р°РІР»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ РёРЅРґРµРєСЃСѓ\n");
-        printf("6. РЈРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ РёРЅРґРµРєСЃСѓ\n");
-        scanf("%d", &option);
-        int input_number, input_index;
-        
-        switch (option)
+        printf("Выберете метод работы с программой 1 или 2:\n");
+        scanf("%d", &action);
+        switch (action)
         {
             case 1:
-                printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ:\n");
-                scanf("%d", &input_number);
-                add_to_head(input_number, deck);
+                FILE *List;
+                int number;
+                List = fopen("Lists.txt", "r");
+                if (List == NULL) 
+                {
+                    printf("Ошибка. Не удалось открыть файл.\n");
+                    return 1; 
+                }
+                printf("Считываем числа из файла:\n");
+
+                Deck *deck = make();
+                while (fscanf(List, "%d", &number) == 1) 
+                {
+                    add_to_end(number, deck);
+                }
                 print_deck(deck);
                 printf("\n");
-                break;
-            case 2:
-                remove_from_head(deck);
+
+                fclose(List);
+                printf("Дек загружен из файла. Выберите действие от 1 до 6:\n");
+                printf("1. Добавление элемента в начало дека\n");
+                printf("2. Удаление элемента из начала дека\n");
+                printf("3. Добавление элемента в конец дека\n");
+                printf("4. Удаление элемента из конца дека\n");
+                printf("5. Добавление элемента по индексу\n");
+                printf("6. Удаление элемента по индексу\n");
+                scanf("%d", &option);
+                int input_number, input_index;
+                
+                switch (option)
+                {
+                    case 1:
+                        printf("Введите число:\n");
+                        scanf("%d", &input_number);
+                        add_to_head(input_number, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 2:
+                        remove_from_head(deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 3:
+                        printf("Введите число:\n");
+                        scanf("%d", &input_number);
+                        add_to_end(input_number, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 4:
+                        remove_from_end(deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 5:
+                        printf("Введите число и индекс через клавишу Энтер:\n");
+                        scanf("%d", &input_number);
+                        scanf("%d", &input_index);
+                        add_by_index(input_number, input_index, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 6:
+                        printf("Введите индекс:\n");
+                        scanf("%d", &input_index);
+                        remove_by_index(input_index, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                }
+                printf("Отсортированный дек:\n");
+                bubble_sort(deck);
                 print_deck(deck);
                 printf("\n");
+                printf("Хотите продолжить программу? 1 - да, 0 - нет\n");
+                scanf("%d", &is_working);
+                if (is_working == 0)
+                {
+                    break;
+                }
                 break;
-            case 3:
-                printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ:\n");
-                scanf("%d", &input_number);
-                add_to_end(input_number, deck);
-                print_deck(deck);
-                printf("\n");
-                break;
-            case 4:
-                remove_from_end(deck);
-                print_deck(deck);
-                printf("\n");
-                break;
-            case 5:
-                printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ Рё РёРЅРґРµРєСЃ С‡РµСЂРµР· РєР»Р°РІРёС€Сѓ Р­РЅС‚РµСЂ:\n");
-                scanf("%d", &input_number);
-                scanf("%d", &input_index);
-                add_by_index(input_number, input_index, deck);
-                print_deck(deck);
-                printf("\n");
-                break;
-            case 6:
-                printf("Р’РІРµРґРёС‚Рµ РёРЅРґРµРєСЃ:\n");
-                scanf("%d", &input_index);
-                remove_by_index(input_index, deck);
-                print_deck(deck);
-                printf("\n");
-                break;
+        
             
-        }
-        printf("РћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ РґРµРє:\n");
-        bubble_sort(deck);
-        print_deck(deck);
-        printf("\n");
-        printf("РҐРѕС‚РёС‚Рµ РїСЂРѕРґРѕР»Р¶РёС‚СЊ РїСЂРѕРіСЂР°РјРјСѓ? 1 - РґР°, 0 - РЅРµС‚\n");
-        scanf("%d", &is_working);
-        if (is_working == 0)
-        {
-            break;
+            case 2:
+                // input_deck(deck);
+                // printf()
+                // printf("1. Добавление элемента в начало дека\n");
+                // printf("2. Удаление элемента из начала дека\n");
+                // printf("3. Добавление элемента в конец дека\n");
+                // printf("4. Удаление элемента из конца дека\n");
+                // printf("5. Добавление элемента по индексу\n");
+                // printf("6. Удаление элемента по индексу\n");
+                // scanf("%d", &option);
+                // int input_number, input_index;
+                
+                // switch (option)
+                // {
+                //     case 1:
+                //         printf("Введите число:\n");
+                //         scanf("%d", &input_number);
+                //         add_to_head(input_number, deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                //     case 2:
+                //         remove_from_head(deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                //     case 3:
+                //         printf("Введите число:\n");
+                //         scanf("%d", &input_number);
+                //         add_to_end(input_number, deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                //     case 4:
+                //         remove_from_end(deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                //     case 5:
+                //         printf("Введите число и индекс через клавишу Энтер:\n");
+                //         scanf("%d", &input_number);
+                //         scanf("%d", &input_index);
+                //         add_by_index(input_number, input_index, deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                //     case 6:
+                //         printf("Введите индекс:\n");
+                //         scanf("%d", &input_index);
+                //         remove_by_index(input_index, deck);
+                //         print_deck(deck);
+                //         printf("\n");
+                //         break;
+                    
+                // }
+                // printf("Отсортированный дек:\n");
+                // bubble_sort(deck);
+                // print_deck(deck);
+                // printf("\n");
+                // printf("Хотите продолжить программу? 1 - да, 0 - нет\n");
+                // scanf("%d", &is_working);
+                // if (is_working == 0)
+                // {
+                //     break;
+                // }
+                // break; 
         }
     }
-    printf("Р’С‹С…РѕРґ РёР· РїСЂРѕРіСЂР°РјРјС‹...\n");
+    printf("Выход из программы...\n");
     return 0;
 }
