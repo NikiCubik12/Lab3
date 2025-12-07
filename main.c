c5a51a3f6aab320c6b08a363e4e7c113b73d6409
NikiCubik12 <nikicubik@gmail.com>
2025-12-07 20:40:19 +0300

    ...

--- main.c
+++ main.c
@@ -118,71 +118,70 @@ int main()
         
             
             case 2:
-                // input_deck(deck);
-                // printf()
-                // printf("1. Добавление элемента в начало дека\n");
-                // printf("2. Удаление элемента из начала дека\n");
-                // printf("3. Добавление элемента в конец дека\n");
-                // printf("4. Удаление элемента из конца дека\n");
-                // printf("5. Добавление элемента по индексу\n");
-                // printf("6. Удаление элемента по индексу\n");
-                // scanf("%d", &option);
-                // int input_number, input_index;
+                input_deck(deck);
+                printf("1. Добавление элемента в начало дека\n");
+                printf("2. Удаление элемента из начала дека\n");
+                printf("3. Добавление элемента в конец дека\n");
+                printf("4. Удаление элемента из конца дека\n");
+                printf("5. Добавление элемента по индексу\n");
+                printf("6. Удаление элемента по индексу\n");
+                scanf("%d", &option);
+                int input_number, input_index;
                 
-                // switch (option)
-                // {
-                //     case 1:
-                //         printf("Введите число:\n");
-                //         scanf("%d", &input_number);
-                //         add_to_head(input_number, deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
-                //     case 2:
-                //         remove_from_head(deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
-                //     case 3:
-                //         printf("Введите число:\n");
-                //         scanf("%d", &input_number);
-                //         add_to_end(input_number, deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
-                //     case 4:
-                //         remove_from_end(deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
-                //     case 5:
-                //         printf("Введите число и индекс через клавишу Энтер:\n");
-                //         scanf("%d", &input_number);
-                //         scanf("%d", &input_index);
-                //         add_by_index(input_number, input_index, deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
-                //     case 6:
-                //         printf("Введите индекс:\n");
-                //         scanf("%d", &input_index);
-                //         remove_by_index(input_index, deck);
-                //         print_deck(deck);
-                //         printf("\n");
-                //         break;
+                switch (option)
+                {
+                    case 1:
+                        printf("Введите число:\n");
+                        scanf("%d", &input_number);
+                        add_to_head(input_number, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 2:
+                        remove_from_head(deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 3:
+                        printf("Введите число:\n");
+                        scanf("%d", &input_number);
+                        add_to_end(input_number, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 4:
+                        remove_from_end(deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 5:
+                        printf("Введите число и индекс через клавишу Энтер:\n");
+                        scanf("%d", &input_number);
+                        scanf("%d", &input_index);
+                        add_by_index(input_number, input_index, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
+                    case 6:
+                        printf("Введите индекс:\n");
+                        scanf("%d", &input_index);
+                        remove_by_index(input_index, deck);
+                        print_deck(deck);
+                        printf("\n");
+                        break;
                     
-                // }
-                // printf("Отсортированный дек:\n");
-                // bubble_sort(deck);
-                // print_deck(deck);
-                // printf("\n");
-                // printf("Хотите продолжить программу? 1 - да, 0 - нет\n");
-                // scanf("%d", &is_working);
-                // if (is_working == 0)
-                // {
-                //     break;
-                // }
-                // break; 
+                }
+                printf("Отсортированный дек:\n");
+                bubble_sort(deck);
+                print_deck(deck);
+                printf("\n");
+                printf("Хотите продолжить программу? 1 - да, 0 - нет\n");
+                scanf("%d", &is_working);
+                if (is_working == 0)
+                {
+                    break;
+                }
+                break; 
         }
     }
     printf("Выход из программы...\n");
